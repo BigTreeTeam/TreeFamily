ea9c47ed156fce2b4fd7f40247e3d6d74a802127
i9400516 <>
2018-08-29 13:23:35 +0800

    Tree Family project create

---
+++ TreeFamily/Controller/CreateAccountViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CreateAccountViewController.swift
+//  TreeFamily
+//
+//  Created by i9400516 on 2018/8/29.
+//  Copyright © 2018年 i9400516. All rights reserved.
+//
+
+import Foundation
---
+++ TreeFamily/Controller/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TreeFamily
+//
+//  Created by i9400516 on 2018/8/29.
+//  Copyright © 2018年 i9400516. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
